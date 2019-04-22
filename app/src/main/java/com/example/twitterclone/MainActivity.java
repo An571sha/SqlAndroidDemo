d9446d9f16c929d49a23380febefe57b38aee86b
an571sha <anim_esh@yahoo.co.in>
2019-04-22 09:28:17 +0300

    ++ Login with email has been added ++

--- app/src/main/java/com/example/twitterclone/MainActivity.java
+++ app/src/main/java/com/example/twitterclone/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.twitterclone;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,14 +23,53 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userName;
-    EditText password;
+    EditText userNameTextField;
+    EditText passwordTextField;
     TextView login;
+    String email;
+    String pass;
     private static final int RC_SIGN_IN = 123;
+    private static final String TAG = "EmailPassword";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        userNameTextField = (EditText) findViewById(R.id.userName);
+        passwordTextField = (EditText) findViewById(R.id.password);
+        login = findViewById(R.id.signUpOrLogIn);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+
+}
 
     public void logIn(View view){
+        email = userNameTextField.getText().toString();
+        pass = passwordTextField.getText().toString();
 
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+
+                    }
+                });
     }
+
+
 
     public void logInFaceBook(View view){
         String id = "https://storied-link-237106.firebaseapp.com/__/auth/handler";
@@ -42,16 +86,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        userName = findViewById(R.id.userName);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.signUpOrLogIn);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-
-    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
